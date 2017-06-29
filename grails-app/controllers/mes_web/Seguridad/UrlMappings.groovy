5606e5034d25e589a50e4a0ecc681ba24e29ac50
raven178 <crashover92@gmail.com>
2017-06-29 18:06:22 -0500

    Agregandas URL mapping para vistas en application.grovy

--- grails-app/controllers/mes_web/Seguridad/UrlMappings.groovy
+++ grails-app/controllers/mes_web/Seguridad/UrlMappings.groovy
@@ -10,12 +10,22 @@ constraints {
             }
         }
             //declarando rutas donde realizaremos las acciones CRUD
+            //Planta
         "/WS_Planta/$id"(controller: "WS_Planta") {
     action = [GET: "show", PUT: "update", DELETE: "delete", POST: "save"]
         }
+        //material
+           "/WS_Material/$id"(controller: "WS_Material") {
+    action = [GET: "show", PUT: "update", DELETE: "delete", POST: "save"]
+        }
+
+
+
 
         "/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
     }
 }
+
+
